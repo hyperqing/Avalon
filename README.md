471dfa44ea649a50d65e86dc9d56775ef10b2006
HyperQing <469379004@qq.com>
2017-07-04 22:57:56 +0800

    添加自述文件

---
+++ README.md
@@ -0,0 +1,6 @@
+# Avalon
+by HyperQing 2017-07-04
+
+>"遗世独立的理想乡"
+
+Workerman 基础服务
--- server.php
+++ server.php
@@ -1,3 +1,21 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
+use Workerman\Connection\TcpConnection;
+use Workerman\Worker;
+
+require_once __DIR__ . '/vendor/workerman/workerman/Autoloader.php';
+
+// 创建一个Worker监听2345端口，使用http协议通讯
+$http_worker = new Worker("http://0.0.0.0:2345");
+
+// 启动4个进程对外提供服务
+$http_worker->count = 4;
+
+// 接收到浏览器发送的数据时回复hello world给浏览器
+$http_worker->onMessage = function (TcpConnection $connection, $data) {
+    // 向浏览器发送hello world
+    $connection->send('hello world');
+};
+
+Worker::runAll();
