f5d6d7e0b98259766a4cc5b663191bdd2189acc0
HyperQing <469379004@qq.com>
2017-07-05 09:47:39 +0800

    websocket监听端口

--- server.php
+++ server.php
@@ -7,13 +7,18 @@ use Workerman\Worker;
 require_once __DIR__ . '/vendor/workerman/workerman/Autoloader.php';
 
 // 创建一个Worker监听2345端口，使用http协议通讯
-$http_worker = new Worker("http://0.0.0.0:2345");
+$worker = new Worker("websocket://0.0.0.0:52233");
 
-// 启动4个进程对外提供服务
-$http_worker->count = 4;
+// 单进程，暂不考虑多进程通信
+$worker->count = 1;
 
 // 接收到浏览器发送的数据时回复hello world给浏览器
-$http_worker->onMessage = function (TcpConnection $connection, $data) {
+$worker->onMessage = function (TcpConnection $connection, $data) {
+    // 向浏览器发送hello world
+    $connection->send('hello world');
+};
+
+$worker->onConnect = function (TcpConnection $connection, $data) {
     // 向浏览器发送hello world
     $connection->send('hello world');
 };
