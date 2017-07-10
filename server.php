44cdbf4ba4e29063ed986d5423989b3894aad9e3
HyperQing <469379004@qq.com>
2017-07-10 14:19:04 +0800

    成功实现用户绑定和一对一发送信息

--- server.php
+++ server.php
@@ -11,27 +11,61 @@ $worker = new Worker("websocket://0.0.0.0:52233");
 
 // 单进程，暂不考虑多进程通信
 $worker->count = 1;
+// 单进程使用的连接对象数组
+$worker->useridList = [];
 
-// 接收到浏览器发送的数据时回复hello world给浏览器
-$worker->onMessage = function (TcpConnection $connection, $data) {
-    // 向浏览器发送hello world
-    var_dump($data);
-    $connection->send("登录成功");
-};
-
-$worker->onConnect = function (TcpConnection $connection) {
-    // 读取连接ws时附带的GET参数
-    $connection->onWebSocketConnect = function ($connection, $http_header) {
-        var_dump($_GET);
-    };
-    // 向浏览器发送hello world
-    $connection->send('用户' . $connection->id . '已上线');
-};
-
+// 工作线程开始
 $worker->onWorkerStart = function (Worker $worker) {
     // 将db实例存储在全局变量中(也可以存储在某类的静态成员中)
     global $db;
-    $db = new Workerman\MySQL\Connection('', '', '', '', '');
+    $db = new Workerman\MySQL\Connection(getenv('MYSQL_HOST'),
+        getenv('MYSQL_PORT'),
+        getenv('MYSQL_USERNAME'),
+        getenv('MYSQL_PASSWORD'),
+        getenv('MYSQL_DBNAME')
+    );
+};
+
+$worker->onConnect = function (TcpConnection $connection) {
+    // 介入握手过程
+    $connection->onWebSocketConnect = function (TcpConnection $connection, $http_header) {
+        // 读取表单
+        $phone = $_GET['phone'];
+        $password = $_GET['password'];
+        // 查询用户
+        global $db;
+        $account = $db->select('user_id,user_phone,user_password,user_name')
+            ->from('oc_account')
+            ->where('user_phone= :phone')
+            ->bindValues(['phone' => $phone])
+            ->row();
+        if (!$account) {
+            $connection->send('用户不存在');
+            $connection->close();
+            return;
+        }
+        if (!\hyperqing\Password::verify($password, $account['user_password'])) {
+            $connection->send('手机或密码错误');
+            $connection->close();
+            return;        }
+        // 登录成功的情况，绑定用户id
+        $connection->user_id = $account['user_id'];
+        $connection->phone = $account['user_phone'];
+        $connection->user_name = $account['user_name'];
+        // 存入连接对象数组，方便使用
+        global $worker;
+        $worker->useridList[$connection->user_id] = $connection;
+    };
+    // 此处不添加代码，按照回调执行顺序，先执行$worker->onConnect，然后$connection->onWebSocketConnect。
+};
+
+// 接收到浏览器发送的数据时回复hello world给浏览器
+$worker->onMessage = function (TcpConnection $connection, $data) {
+    // 测试假设消息内只有userid，则将本次消息投递到该id客户端中
+    global $worker;
+    if (isset($worker->useridList[$data])) {
+        $worker->useridList[$data]->send('来自 ' . $connection->user_id . ' 的信息: ' . $data);
+    }
 };
 
 Worker::runAll();
